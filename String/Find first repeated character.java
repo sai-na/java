3973627ffba564440e3b1a90a1c0a950acf805ca
sai-na <saipkmm100@gmail.com>
2023-09-12 22:59:00 +0530

    Find first repeated character

---
+++ String/Find first repeated character.java
@@ -0,0 +1,32 @@
+package String;
+
+// Find first repeated character
+
+import java.util.Scanner;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    String userInput;
+    boolean flag = false;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter a String");
+    userInput = scanner.nextLine();
+    String uniqueString = "";
+
+    for (int i = 0; i < userInput.length(); i++) {
+      if (uniqueString.indexOf(userInput.charAt(i)) == -1) {
+        uniqueString += userInput.charAt(i);
+        continue;
+      } else {
+        System.out.println(userInput.charAt(i));
+        flag = true;
+        break;
+      }
+    }
+
+    if (!flag) {
+      System.out.println("NO repeated characters");
+    }
+  }
+}
